ef9b82da52540d3dc4ae9bfd1841f92e85dedaa0
Varuna <jakkanavaruna19@gmail.com>
2026-06-05 12:07:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -388,7 +388,7 @@ def predict():
 # 7. RUN SERVER
 # ==============================
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=False)
 
 
         
